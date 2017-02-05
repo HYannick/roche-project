7074df65972f1a3c77c81a7d0723fe503947c5d3
yhoussin <yhoussin@sqli.com>
2017-02-05 11:55:20 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+
+
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(path.join(__dirname, '/')));
+
+
+
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
